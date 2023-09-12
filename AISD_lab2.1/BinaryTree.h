9b6979cdd734e4f2765b3beda33912f17a1b786b
kvasbulbas <shamonin.2002@mail.ru>
2023-09-13 02:26:48 +0700

    Настройка класса
    Добавил конструкторы, деструкторы, структуру Node

--- AISD_lab2.1/BinaryTree.h
+++ AISD_lab2.1/BinaryTree.h
@@ -1,7 +1,29 @@
 #pragma once
 class BinaryTree
 {
-	int a;
+public:
+	struct Node;
+	BinaryTree() = default;
+	virtual ~BinaryTree();
 
+
+protected:
+	Node* _root = nullptr;
+};
+
+
+struct BinaryTree::Node
+{
+	int key = 0;
+	Node* left = nullptr;
+	Node* right = nullptr;
+	Node(int k = 0, Node* l = nullptr, Node* r = nullptr)
+	{
+		key = k;
+		left = l;
+		right = r;
+	}
+
+	~Node() {};
 };
 
