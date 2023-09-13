ddeeecca71129d6c7f0dc5e9ccdab46d5ef0ce47
kvasbulbas <shamonin.2002@mail.ru>
2023-09-13 18:51:02 +0700

    Скопировал код из старых файлов и вставил в новые

---
+++ AISD_lab2.1/BinaryTreeTester.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "BinaryTree.h"
+
+class BinaryTreeTester
+{
+public:
+    BinaryTreeTester(const bool useConsoleOutput);
+
+    ~BinaryTreeTester() = default;
+
+    void test(const int size);
+
+protected:
+    virtual BinaryTree* allocateTree();
+    virtual void check_addAndCount(const BinaryTree* tree, const int size);
+
+    ////virtual void check_remove(const BinaryTree* tree, const int size);
+    virtual void check_clear(const BinaryTree* tree, const int size);
+    virtual void check_assign(const BinaryTree& tree1, const BinaryTree& tree2);
+    virtual void check_assignOfNode(const BinaryTree::Node* node1, const BinaryTree::Node* node2);
+    virtual void check_height(const BinaryTree& tree, const int size);
+    virtual void check_deleteNodeByKey(const BinaryTree* tree, const int size);
+
+
+
+private:
+    void deleteNodeByKey();
+    void deallocateTree(BinaryTree* tree);
+    void addAndCount();
+    void destructor();
+    void clear(); //ToDo: реализовать
+    void assign();
+    void height();
+    void height_trivialCases();
+
+
+private:
+    int m_maxSize;
+    bool m_useConsoleOutput;
+
+
+};
